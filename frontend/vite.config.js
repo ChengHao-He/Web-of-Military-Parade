f8a927f1afd228bfaeb76a17095afe885e2a9666
chenghaoHe <935684256@qq.com>
2025-09-05 11:24:49 +0800

    new front feature

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,18 +1,35 @@
-import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import { truncate } from 'fs'
 
-// https://vite.dev/config/
+const { resolve } = require('path')
+
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  build:{
+    chunkSizeWarningLimit:1500,
+  },
+   // 起个别名，在引用资源时，可以用‘@/资源路径’直接访问
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      "@": resolve(__dirname, "src"),
     },
   },
+  plugins: [vue()],
+  // 配置前端服务地址和端口
+  server: {
+    host: '0.0.0.0',
+    port: 443,
+    // 是否开启 https
+    https: false,
+  },
+  // 设置反向代理，跨域
+  proxy: {
+    '/api1': {
+      target: 'https://www.ichlearning.cn/',
+      changeOrigin: true,
+      https: true,
+      rewrite: path => path.replace(/^\/api1/, '')
+    }
+  }
 })
+
