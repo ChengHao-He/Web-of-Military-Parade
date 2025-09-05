25dd59d642adcafcedd463826c79388fd5280a4e
Ssytt8 <2012526@mail.nankai.edu.cn>
2025-09-05 11:30:30 +0800

    new file:   backend/backend/SE_back-end/src/main/java/com/se_backend/Interceptor/IPInterceptor.java 	new file:   backend/backend/SE_back-end/src/main/java/com/se_backend/Interceptor/InterceptorMapper/InterceptorRolePowerMapper.java 	new file:   backend/backend/SE_back-end/src/main/java/com/se_backend/Interceptor/InterceptorMapper/InterceptorURLPowerNeedMapper.java 	new file:   backend/backend/SE_back-end/src/main/java/com/se_backend/Interceptor/InterceptorMapper/InterceptorUerRoleMapper.java 	new file:   backend/backend/SE_back-end/src/main/java/com/se_backend/Interceptor/PowerInterceptor.java 	new file:   backend/backend/SE_back-end/src/main/java/com/se_backend/Interceptor/RequestBodyFilter.java 	new file:   backend/backend/SE_back-end/src/main/java/com/se_backend/Interceptor/RequestReaderHttpServletRequestWrapper.java 	new file:   backend/backend/SE_back-end/src/main/java/com/se_backend/Interceptor/UserPowerCheck.java 	new file:   backend/backend/SE_back-end/src/main/java/com/se_backend/Projects/PersonalArticlesProject/PersonalArticles_Controller/PersonalArticlesController.java 	new file:   backend/backend/SE_back-end/src/main/java/com/se_backend/Projects/PersonalArticlesProject/PersonalArticles_Object/AddPage.java 	new file:   backend/backend/SE_back-end/src/main/java/com/se_backend/Projects/Video_project/Video_Controller/Video_ShowController.java 	new file:   frontend/src/home.vue 	new file:   frontend/src/page/ManageComponents/ManageAbout.vue 	new file:   frontend/src/page/ManageComponents/ManagePersonPage.vue

---
+++ backend/backend/SE_back-end/src/main/java/com/se_backend/Interceptor/InterceptorMapper/InterceptorUerRoleMapper.java
@@ -0,0 +1,17 @@
+package com.se_backend.Interceptor.InterceptorMapper;
+
+import com.se_backend.DB.DB_object.UserRole;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.ArrayList;
+
+@Mapper
+public interface InterceptorUerRoleMapper {
+    @Select("Select * from UserRole where UserID=#{UserID}")
+    ArrayList<UserRole> getUserAllRole(@Param("UserID")String UsrID);
+
+    @Select("Select name from Role where id=#{id} limit 1")
+    String getUserRoleName(@Param("id")Integer id);
+}
